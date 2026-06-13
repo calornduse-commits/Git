668f672d72d0856f3696b9db7e9a90e0906bd3a9
calornduse-commits <calornduse@gmail.com>
2026-06-13 17:48:21 +0530

    modified

--- calc.py
+++ calc.py
@@ -1,6 +1,5 @@
 print("Hello, World! This is a simple calculator program.")
-def add(x, y):
-    return x + y
+def add(x, y):     return x + y
 def subtract(x, y):    return x - y
 def multiply(x, y):    return x * y
 def divide(x, y):      return x / y
